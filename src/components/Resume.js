6f3ce1512dcdfb957d83dc2a063ca50190970b6a
pasupathi07777 <pasupathi07777@gamil.com>
2024-04-22 03:24:06 +0530

    ll

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -33,7 +33,7 @@ const Resume = () => {
           <div className='flex flex-col justify-center text-white text-center'>
             <h1 className='w-[150px] text-4xl text-white border-b-4 mx-auto  border-bg-color-2  font-bold   mb-4'>{resume.title}</h1>
             <p className='flex -flex-col sm:flex-row gap-5'>
-              <Link onClick={openPdfInNewTab} to={'/resume'} > <p className='btn hover:border-2 bg-color-2 rounded border-white min-w-[120px] text-center' >{resume.View_btn}</p></Link>
+              <button onClick={openPdfInNewTab}  > <p className='btn hover:border-2 bg-color-2 rounded border-white min-w-[120px] text-center' >{resume.View_btn}</p></button>
               <a onClick={downloadPdf} className='btn hover:border-2 bg-color-2 rounded border-white min-w-[120px] text-center' download='Pasupathi_Resume' href={resume.pdf}>{resume.Downlode_btn}</a>
 
             </p>
